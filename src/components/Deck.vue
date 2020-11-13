1680d4e5be46d46b4c4adf6a66cd8ac40898680f
Culdred <elia.gentilucci@gmail.com>
2020-11-13 16:44:57 +0100

    create Deck component

---
+++ src/components/Deck.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Deck Component</h2>
+        <button @click="showDeckCreate = !showDeckCreate" type="button">
+            Create New Deck
+        </button>
+        <form v-if="showDeckCreate">
+            <Card />
+        </form>
+    </div>
+</template>
+
+<script>
+import Card from "./Card.vue"
+
+export default {
+    name: "Deck",
+    components: {
+        Card,
+    },
+    data: () => ({
+        showDeckCreate: false,
+    }),
+}
+</script>
