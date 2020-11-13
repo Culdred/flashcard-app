61e09751d721f24c322296d65427fb6452fc6726
Culdred <elia.gentilucci@gmail.com>
2020-11-13 16:44:46 +0100

    remove default stuff from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Flashcards App</h1>
+    <Deck />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Deck from './components/Deck.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Deck
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
